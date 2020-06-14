ddd72846a0f9856f786ff49cf3c523b3dd6ac702
Özgür Koca <ozgurkoca@gmail.com>
2020-06-14 03:14:18 +0300

    Program renamed. ss963.service to spi.service

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 clear
 # Stop the service
-printf "> Stopping ss963.service...\n"
-sudo systemctl stop ss963.service
-sudo systemctl kill ss963.service
+printf "> Stopping spi.service...\n"
+sudo systemctl stop spi.service
+sudo systemctl kill spi.service
 
 # Remove old binary
-sudo rm -rf /usr/sbin/ss963
-if [ ! -f /usr/sbin/ss963 ]; then
+sudo rm -rf /usr/sbin/spiservice
+if [ ! -f /usr/sbin/spiservice ]; then
     printf "> Old binary removed.\n"
 else
 	printf "\n\n> ERROR: Old binary was not removed.\n\n"
@@ -15,23 +15,23 @@ fi
 
 # Compile and copy to target location /usr/sbin/
 printf "> Compiling from source code...\n"
-sudo gcc -o /usr/sbin/ss963 ss963.c -lwiringPi -Ofast -s -Wno-implicit
+sudo gcc -o /usr/sbin/spiservice spi_service.c -lwiringPi -Ofast -s -Wno-implicit
 
-if [ -f /usr/sbin/ss963 ]; then
+if [ -f /usr/sbin/spiservice ]; then
 	printf "> New binary created."
 fi
 
 # Remove old service file
-sudo rm -rf /lib/systemd/system/ss963.service
-if [ ! -f /lib/systemd/system/ss963.service ]; then
+sudo rm -rf /lib/systemd/system/spi.service
+if [ ! -f /lib/systemd/system/spi.service ]; then
     printf "\n> Old SystemD unit file removed.\n"
 else
 	printf "\n\n> ERROR: Old SystemD unit file was not removed.\n\n"
 fi
 
 # Remove conf file
-sudo rm -rf /etc/ss963.service.conf
-if [ ! -f /etc/ss963.service.conf ]; then
+sudo rm -rf /etc/spi.service.conf
+if [ ! -f /etc/spi.service.conf ]; then
     printf "> Old configuration file removed.\n"
 else
 	printf "\n\n> ERROR: Old configuration file was not removed.\n\n"
@@ -40,21 +40,21 @@ fi
 # Install SystemD service
 printf "> New SystemD unit file installing...\n"
 
-sudo cp ss963.service.conf.template /etc/ss963.service.conf
+sudo cp spi.service.conf.template /etc/spi.service.conf
 
-if [ -f /etc/ss963.service.conf ]; then
-	printf "> New configuration file created at /etc/ss963.service.conf with default settings.\n"
+if [ -f /etc/spi.service.conf ]; then
+	printf "> New configuration file created at /etc/spi.service.conf with default settings.\n"
 else
-	printf "\n\n> ERROR: New configuration file was not created at /etc/ss963.service.conf.\n"
+	printf "\n\n> ERROR: New configuration file was not created at /etc/spi.service.conf.\n"
 fi
 
-sudo cp ss963.service.template /lib/systemd/system/ss963.service
+sudo cp spi.service.template /lib/systemd/system/spi.service
 sudo systemctl daemon-reload
-sudo systemctl enable ss963.service
-sudo systemctl unmask ss963.service
+sudo systemctl enable spi.service
+sudo systemctl unmask spi.service
 
-printf "> The service is ss963.service starting...\n\n"
-sudo systemctl start ss963.service
-sudo systemctl status ss963.service
+printf "> The service is spi.service starting...\n\n"
+sudo systemctl start spi.service
+sudo systemctl status spi.service
 
 printf "\nFinished.\n"
